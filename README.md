52572cd671f61e0e53f78e0f02250eb389573456
Vishal Shekhar <114502901+afterallvishal@users.noreply.github.com>
2026-07-14 18:25:14 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,48 @@
+# Bangalore House Price Predictor
+
+A Machine Learning based web application that predicts Bangalore house prices based on user inputs such as location, total square feet, number of bathrooms and BHK.
+
+## Project Overview
+
+This project uses Machine Learning regression techniques to predict house prices in Bangalore. A complete ML pipeline is created including data preprocessing, feature engineering, model training and deployment using Streamlit.
+
+## Technologies Used
+
+- Python
+- Pandas
+- NumPy
+- Scikit-Learn
+- Matplotlib
+- Seaborn
+- Streamlit
+
+## Machine Learning Workflow
+
+- Data Cleaning
+- Exploratory Data Analysis (EDA)
+- Feature Engineering
+- Data Preprocessing
+- Model Training
+- Model Evaluation
+- Deployment using Streamlit
+
+## Features Used
+
+- Location
+- Total Square Feet
+- Number of Bathrooms
+- BHK (Bedrooms)
+
+## Model Used
+
+- Ridge Regression
+
+## Model Pipeline
+
+The model uses:
+
+- OneHotEncoder for categorical feature (Location)
+- StandardScaler for feature scaling
+- Ridge Regression for prediction
+
+
---
+++ app.py
@@ -0,0 +1,60 @@
+import streamlit as st
+import pickle
+import pandas as pd
+
+
+
+
+
+#load model
+
+model=pickle.load(open("RidgeModel.pkl",'rb'))
+
+#load locations
+locations = pickle.load(open('locations.pkl', 'rb'))
+
+st.title("Bangalore House Price Prediction")
+
+st.write("Enter the Details to Predict the Estimated Price")
+
+#user input
+
+location=st.text_input('Enter Location').strip()
+
+total_sqft= st.number_input('Total Square Feet',min_value=450,
+                            help='For better predictions, try values above 450')
+st.info("For better predictions, enter Total Square Feet of at least 450.")
+
+bath=st.number_input('Number of Bathrooms',min_value=1)
+
+bhk=st.number_input('Number of BHK', min_value=1)
+
+#prediction
+
+if st.button('Predict Price'):
+
+    input_data=pd.DataFrame({
+        'location':[location],
+        'total_sqft':[total_sqft],
+        'bath':[bath],
+        'bhk':[bhk]
+    })
+
+    if location not in locations:
+         st.warning("Location not found. Please enter a valid Bangalore location.")
+    try:
+
+        prediction=model.predict(input_data)
+        Price=prediction[0]
+
+        # Negative prediction handling
+        if Price < 0:
+            st.warning("Please enter a realistic house size and location.")
+        elif Price >= 100:
+            price_crores = Price / 100
+            st.success(f'Estimated Price : ₹{price_crores:.2f} Crores')
+        else:
+            st.success(f'Estimated House Price : ₹{Price:.2f} Lakhs')
+
+    except ValueError:
+        st.error("Invalid Location ! Please enter a Valid Location")
